52de17db4a1d2de09426467d2d3cc66805fc0d35
jaankiseema <seemadev.jankiwebinfo@gmail.com>
2025-07-22 23:28:46 +0530

    new

--- src/sections/Carearpage.jsx
+++ src/sections/Carearpage.jsx
@@ -49,7 +49,7 @@ const Carearpage = () => {
               CAREER
             </h1>
             <p className="text-md md:text-xl font-light">
-              Join our passionate team and grow your future with usyes.
+              Join our passionate team and grow your future with usyes.j yes i run the ai
             </p>
           </div>
         </div>
